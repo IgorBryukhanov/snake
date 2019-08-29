349475d0a0af4335758b02295804ab4e26675af3
igor-901 <igor.kz09@mail.ru>
2019-08-29 22:01:26 +0300

    upravlenie

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -42,12 +42,12 @@ namespace snake
             else if
                 (direction == Direction.UP)
             {
-                y = y + offset;
+                y = y - offset;
             }
             else if
                 (direction == Direction.DOWN)
             {
-                y = y - offset;
+                y = y + offset;
             };
         }
             
